46d973365322ceb0d92e651b42b1e23bca17972d
clamper1223 <clamper1223@naver.com>
2022-09-16 14:04:08 +0900

    HelloSDLsepa
    실습
    추가실습

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,62 @@
 ﻿#include <SDL2/SDL.h>
+#include <stdlib.h>
+#include <time.h>
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
+bool g_bRunning = false;
 
-int main(int argc, char* args[])
+bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
 {
+    if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
 
-    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
-
-        g_pWindow = SDL_CreateWindow("Setting up SDL",
-            SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED, 640, 480,
-            SDL_WINDOW_SHOWN);
+        g_pWindow = SDL_CreateWindow(
+            title, xpos, ypos, height, width, flags);
 
         if (g_pWindow != 0) {
-            g_pRenderer
-                = SDL_CreateRenderer(g_pWindow, -1, 0);
+            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
         }
+
     }
     else {
-        return 1;
+        return false;
     }
 
-    SDL_SetRenderDrawColor(
-        g_pRenderer, 0, 0, 0, 255);
+    //SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+    SDL_SetRenderDrawColor(g_pRenderer, rand() % 256, rand() % 256, rand() % 256, 255);
+    SDL_Delay(1000);
+
+    return true;
+}
+
+void render()
+{
     SDL_RenderClear(g_pRenderer);
+
+    // 그리기 수행  
+
     SDL_RenderPresent(g_pRenderer);
+}
 
-    SDL_Delay(5000);
+int main(int argc, char* argv[])
+{
+    if (init("Breaking Up HelloSDL", SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, 640, 480,
+        SDL_WINDOW_SHOWN))
+    {
+        g_bRunning = true;
+    }
+    else
+    {
+        return 1; // something's wrong
+    }
+
+    while (!g_bRunning)
+    {
+        // handle input - update - render
+        render();
+    }
+
     SDL_Quit();
-
     return 0;
 }
