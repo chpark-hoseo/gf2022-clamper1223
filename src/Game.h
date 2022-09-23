680bad18e49dfaf8d76bba456028af5f5cc8170a
RynT <clamper1223@naver.com>
2022-09-23 14:23:20 +0900

    SDL 기본 그리기 실습 미완

--- src/Game.h
+++ src/Game.h
@@ -1,1 +1,15 @@
 #pragma once
+#include <main.h>
+SDL_Texture* m_pTexture;
+
+// 원본 사각형
+SDL_Rect m_sourceRectangle;
+
+// 대상 사각형
+SDL_Rect m_destinationRectangle;
+
+typedef struct SDL_Rect
+{
+	int x, y;
+	int w, h;
+} SDL_Rect;
