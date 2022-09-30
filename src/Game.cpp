39880c3a21dcf3bcfa7cef45b0710a1502bb4890
RynT <clamper1223@naver.com>
2022-09-30 13:48:30 +0900

    애니메이션 스프라이트 실습2

--- src/Game.cpp
+++ src/Game.cpp
@@ -56,7 +56,7 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, in
 }
 void Game::update()
 {
-
+    m_sourceRectangle.x = 128 * ((SDL_GetTicks() / 100) % 6);
 }
 void Game::render()
 {
