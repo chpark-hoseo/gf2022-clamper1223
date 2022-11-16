984257acb5527d39a708933280f4d49b64255746
RynT <clamper1223@naver.com>
2022-11-16 16:57:54 +0900

    실습

--- src/SDLGameObject.h
+++ src/SDLGameObject.h
@@ -4,18 +4,19 @@
 #include <string>
 #include "GameObject.h"
 #include "LoaderParams.h"
+#include "Vector2D.h"
 
 class SDLGameObject : public GameObject {
 public:
 	SDLGameObject(const LoaderParams* pParams);
 	virtual void draw();
-	virtual void update() {}
+	virtual void update();
 	virtual void clean() {}
 	virtual ~SDLGameObject() {}
 
 protected:
-	int m_x;
-	int m_y;
+
+	Vector2D m_position;
 	int m_width;
 	int m_height;
 	int m_currentRow;
